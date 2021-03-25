5d927807546b8cbcc7751bb2968b033fcdce1fa0
1gilmar <1gilmar@gmail.com>
2021-03-25 15:55:45 -0300

    ajustado para acessar a ela mesma

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const rodar = require('./app')
 const express = require('express')
 
+const status = require('./status')
 
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-// const agendar = require('node-schedule')
-// let job;
-// job = agendar.scheduleJob('* * * * * *', ()=>{
-//   console.log("rodando ...");
-// })
 
 //! Para rodar o boot
 rodar()
+
+const agendar = require('node-schedule')
+let job;
+job = agendar.scheduleJob('1 * * * * *', async () => {
+  try {
+    console.log("rodando ... ...");
+    const response = await status()
+
+    if (response.status == 200) {
+      console.log("ok", Date())
+    } else {
+      console.log("nao ok")
+    }
+  } catch (error) {
+    console.log(error)
+  }
+
+})
 
 console.log("api ira acessar ela mesmo aqui.")
 
 express()
   .get('/', (req, res) => res.send('<div id="nome">rodando o bot ...</div>'))
-  .get('/api', (req, res)=> res.json({bot: "agora"}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/api', (req, res) => res.json({ bot: "agora roda acessando a si mesmo" }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
--- status.js
+++ status.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const status = async () => {
     try {
-        return await axios.get('https://www.google.com/', { timeout: 30000 })
+        return await axios.get('https://bot-jiraspiom.herokuapp.com/', { timeout: 30000 })
     } catch(error) {
         return error
     }
