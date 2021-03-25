ec12378dc75c0bb4f74546da01ae24b79fe4b4d1
1gilmar <1gilmar@gmail.com>
2021-03-25 15:33:35 -0300

    testando

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const rodar = require('./app')
 const express = require('express')
-// const agendar = require('node-schedule')
+
 
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// const agendar = require('node-schedule')
 // let job;
-
-
 // job = agendar.scheduleJob('* * * * * *', ()=>{
 //   console.log("rodando ...");
 // })
 
-
 //! Para rodar o boot
 rodar()
+
+console.log("api ira acessar ela mesmo aqui.")
 
 express()
   .get('/', (req, res) => res.send('<div id="nome">rodando o bot ...</div>'))
