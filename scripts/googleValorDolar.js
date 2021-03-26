6e0d504c6733c0361d73a9dcdbafededd659f750
1gilmar <1gilmar@gmail.com>
2021-03-26 19:37:15 -0300

    criado api do google para retornar o valor do dolar

---
+++ scripts/googleValorDolar.js
@@ -0,0 +1,22 @@
+const cheerio = require('cheerio')
+const { api } = require('../api/api_axios')
+
+const url = 'https://www.google.com/search?q=dolar+para+real&oq=dolar+para+real&aqs=chrome.0.69i59j0l7.1726j0j4&sourceid=chrome&ie=UTF-8'
+
+function google() {
+
+	api.get(url).then(response => {
+
+		var seletor = cheerio.load(response.data)
+
+		const resultado = seletor("body").find('.BNeawe.iBp4i.AP7Wnd')
+		
+		return resultado[1].childNodes[0].data
+
+	}).catch(erro => {
+		console.log("deu erro", erro)
+	})
+
+}
+
+module.exports = google;
