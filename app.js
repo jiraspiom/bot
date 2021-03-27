3171dc80bbcf6c92a9abbd9db0c62720f0890a54
1gilmar <1gilmar@gmail.com>
2021-03-27 00:06:06 -0300

    adicionado mais moedas

--- app.js
+++ app.js
@@ -10,48 +10,35 @@ let status_job = false
 
 const rodar = () => {
 	const bot = new Telegraf(process.env.BOT_TOKEN)
-	bot.start((ctx) => ctx.reply('Welcome'))
+	bot.start((ctx) => ctx.reply('Welcome ao bot de teste...'))
 	bot.help((ctx) => ctx.reply(`Digite "Ajuda" para visualizar os comandos`))
 
 	bot.on('sticker', (ctx) => ctx.reply('👍'))
 
 	bot.hears('Oi', (ctx) => ctx.reply('E ai?, carinha que mora logo ali!'))
+	bot.hears('Jiraspim', (ctx) => ctx.reply('Qualquer coisa?, Qualquer lugar!'))
 	bot.hears(/sobre/i, (ctx) => ctx.reply('Este bot é apenas um teste'))
+
+	bot.hears(/bolsonaro/i, (ctx) => ctx.reply('Presitente'))
+	bot.hears(/lula/i, (ctx) => ctx.reply('ex-Presidente'))
+
+	bot.hears(/42/i, (ctx) => ctx.reply('A resposta para a vida o universo e tudo mas.'))
 
 	bot.hears(/ping/i, ctx => {
 		ctx.reply('Pong!')
 	})
 
 	bot.hears(/ajuda/i, ctx => {
-		ctx.reply(`comandos:
-    /ligar - ligar o bot
-    /desligar - delisga o bot
-    /status - status do ligado o desligado`)
-	})
-
-	bot.hears(/dolar/i, async ctx => {
-		await google('dolar').then(valor => {
-			ctx.reply(valor)
-		}).catch(erro => {
-			console.log('erro ao buscar a funcao dolar', erro)
-		})
-	})
-
-	bot.hears(/libra/i, async ctx=>{
-		await google('libra').then(valor => {
-			ctx.reply(valor.texto)
-		}).catch(erro =>{
-			console.log('erro ao buscar a funcao euro', erro)
-		})
+		ctx.reply(`--comandos-- \n /ligar - ligar o bot \n /desligar - delisga o bot \n /status - status do ligado o desligado`)
 	})
 
 	bot.command('ligar', async (ctx) => {
 		try {
 			job = agendar.scheduleJob('* * * * * *', async () => {
-				console.log("rodando ...", Date());
+				console.log("rodando rastreio de dolar...", Date());
 				status_job = true
-				await google.then(valor => {
-					ctx.reply(`rastreado ligado - R$ \n ${valor}`)
+				await google('dolar').then(valor => {
+					ctx.reply(`rastreado ligado - R$ \n ${valor.resultado}`)
 				}).catch(erro => {
 					console.log("erro ao ligar o rastreador");
 					console.log(erro)
@@ -80,6 +67,40 @@ const rodar = () => {
 		} else {
 			await ctx.reply('rastreador está desligado')
 		}
+	})
+
+
+	//* Moedas estão aqui abaixo
+	bot.hears(/dolar/i, async ctx => {
+		await google('dolar').then(valor => {
+			ctx.reply(valor.texto)
+		}).catch(erro => {
+			console.log('erro ao buscar a funcao dolar', erro)
+		})
+	})
+
+	bot.hears(/libra/i, async ctx=>{
+		await google('libra').then(valor => {
+			ctx.reply(valor.texto)
+		}).catch(erro =>{
+			console.log('erro ao buscar a funcao libra', erro)
+		})
+	})
+
+	bot.hears(/euro/i, async ctx=>{
+		await google('euro').then(valor => {
+			ctx.reply(valor.texto)
+		}).catch(erro =>{
+			console.log('erro ao buscar a funcao euro', erro)
+		})
+	})
+
+	bot.hears(/bitcoin/i, async ctx=>{
+		await google('bitcon').then(valor => {
+			ctx.reply(valor.texto)
+		}).catch(erro =>{
+			console.log('erro ao buscar a funcao bitcon', erro)
+		})
 	})
 
 	bot.launch()
